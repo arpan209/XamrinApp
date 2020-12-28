bfe24f31b1e4256b600c139a2c882f4ff7eba672
arpan209 <arpan209@gmail.com>
2020-12-27 21:56:15 -0500

    Commit all

--- TestApp1/TestApp1/App.xaml.cs
+++ TestApp1/TestApp1/App.xaml.cs
@@ -11,7 +11,7 @@ namespace TestApp1
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new GreetPage();
         }
 
         protected override void OnStart()
